b9767789a58b91678f109895952d8925128b62d8
JosePrietoPaez <0610904705@uma.es>
2024-03-04 20:00:42 +0100

    Primera prueba
    Para asegurarme de que funcionan las dependencias

--- src/test/java/bank/BankAccountTest.java
+++ src/test/java/bank/BankAccountTest.java
@@ -5,4 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountTest {
 
+    private BankAccount cuenta;
+
+    @Test
+    public void Constructor_CuentaInicializadaTieneElSaldoIndicado(){
+        // Assert
+        int cantidadIntroducida = 100;
+        cuenta = new BankAccount(100);
+
+        // Act
+        int cantidadObtenida = cuenta.getBalance();
+
+        // Assert
+        assertEquals(cantidadIntroducida,cantidadObtenida,"Devuelve una cantidad distinta a la introducida");
+    }
+
 }
